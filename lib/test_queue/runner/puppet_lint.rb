9e2f376f54ebbd4582db7e16c5ac846a83a01e0b
Aman Gupta <aman@tmm1.net>
2013-04-24 02:19:46 -0700

    PuppetLint runner

---
+++ lib/test_queue/runner/puppet_lint.rb
@@ -0,0 +1,32 @@
+require 'test_queue'
+require 'puppet-lint'
+
+module TestQueue
+  class Runner
+    class PuppetLint < Runner
+      def run_worker(iterator)
+        errors = 0
+        linter =  PuppetLint.new
+        iterator.each do |file|
+          puts "Evaluating #{file}"
+          linter.file = file
+          linter.run
+          errors += 1 if linter.errors?
+        end
+        errors
+      end
+
+      def summarize_worker(worker)
+        lines = worker.lines
+
+        files    = lines.select{ |line| line =~ /^Evaluating/ }
+        errors   = lines.select{ |line| line =~ /^ERROR/ }
+        warnings = lines.select{ |line| line =~ /^WARNING/ }
+
+        summary = "#{files.size} files, #{warnings.size} warnings, #{errors.size} errors"
+
+        [summary, errors.join("\n")]
+      end
+    end
+  end
+end
