d6297a951b9a51a8f2603be1f1fefaca6b6445c5
Aman Gupta <aman@tmm1.net>
2014-06-23 20:00:15 -0700

    add tests for minitest5

--- test.sh
+++ test.sh
@@ -3,9 +3,15 @@ set -x
 
 export TEST_QUEUE_WORKERS=2 TEST_QUEUE_VERBOSE=1
 
+bundle install --gemfile=Gemfile-minitest4
+bundle exec minitest-queue ./test/*_minitest4.rb
 bundle exec minitest-queue ./test/*_minispec.rb
-bundle exec minitest-queue ./test/*_test.rb
+
+bundle install
+bundle exec minitest-queue ./test/*_minitest4.rb
+bundle exec minitest-queue ./test/*_minitest5.rb
+bundle exec minitest-queue ./test/*_minispec.rb
 bundle exec rspec-queue test
 bundle exec cucumber-queue
 
-TEST_QUEUE_WORKERS=1 TEST_QUEUE_FORCE="MiniTestSleep21,MiniTestSleep8,MiniTestFailure" bundle exec minitest-queue ./test/*_test.rb
+TEST_QUEUE_WORKERS=1 TEST_QUEUE_FORCE="MiniTestSleep21,MiniTestSleep8,MiniTestFailure" bundle exec minitest-queue ./test/*_minitest5.rb
