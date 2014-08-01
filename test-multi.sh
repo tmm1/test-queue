53d9feef60f3f5764dda16516797911849e3b8f4
Ben Lavender <bhuga@github.com>
2014-08-01 14:53:48 -0500

    Make test-multi.sh work again

--- test-multi.sh
+++ test-multi.sh
@@ -2,7 +2,7 @@
 set -x
 
 # export TEST_QUEUE_VERBOSE=1
-TEST_QUEUE_SOCKET=0.0.0.0:12345 bundle exec minitest-queue ./test/sample_test.rb &
+TEST_QUEUE_SOCKET=0.0.0.0:12345 bundle exec minitest-queue ./test/sample_minitest5.rb &
 sleep 0.1
-TEST_QUEUE_RELAY=0.0.0.0:12345  bundle exec minitest-queue ./test/sample_test.rb
+TEST_QUEUE_RELAY=0.0.0.0:12345  bundle exec minitest-queue ./test/sample_minitest5.rb
 wait
