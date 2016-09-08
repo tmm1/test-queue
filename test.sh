749de9035336d786be7156b62a7f007ee04d6c02
Adam Roben <adam@roben.org>
2016-09-08 09:21:56 -0400

    Move sample tests into test/samples
    This will keep them separated from our own tests.

--- test.sh
+++ test.sh
@@ -5,19 +5,19 @@ export TEST_QUEUE_WORKERS=2 TEST_QUEUE_VERBOSE=1
 
 export BUNDLE_GEMFILE=Gemfile-testunit
 bundle install
-bundle exec testunit-queue ./test/*_testunit.rb
+bundle exec testunit-queue ./test/samples/*_testunit.rb
 
 export BUNDLE_GEMFILE=Gemfile-minitest4
 bundle install
-bundle exec minitest-queue ./test/*_minitest4.rb
-bundle exec minitest-queue ./test/*_minispec.rb
+bundle exec minitest-queue ./test/samples/*_minitest4.rb
+bundle exec minitest-queue ./test/samples/*_minispec.rb
 
 export BUNDLE_GEMFILE=Gemfile
 bundle install
-bundle exec minitest-queue ./test/*_minitest4.rb
-bundle exec minitest-queue ./test/*_minitest5.rb
-bundle exec minitest-queue ./test/*_minispec.rb
-bundle exec rspec-queue test
-bundle exec cucumber-queue
+bundle exec minitest-queue ./test/samples/*_minitest4.rb
+bundle exec minitest-queue ./test/samples/*_minitest5.rb
+bundle exec minitest-queue ./test/samples/*_minispec.rb
+bundle exec rspec-queue test/samples
+bundle exec cucumber-queue test/samples/features
 
-TEST_QUEUE_WORKERS=1 TEST_QUEUE_FORCE="MiniTestSleep21,MiniTestSleep8,MiniTestFailure" bundle exec minitest-queue ./test/*_minitest5.rb
+TEST_QUEUE_WORKERS=1 TEST_QUEUE_FORCE="MiniTestSleep21,MiniTestSleep8,MiniTestFailure" bundle exec minitest-queue ./test/samples/*_minitest5.rb
--- test/testunit.bats
+++ test/testunit.bats
@@ -8,7 +8,7 @@ assert_output_contains() {
 }
 
 @test "testunit-queue fails when a test fails" {
-  run bundle exec testunit-queue ./test/*_testunit.rb
+  run bundle exec testunit-queue ./test/samples/*_testunit.rb
   [ "$status" -eq 1 ]
   assert_output_contains "Starting test-queue master"
 }
