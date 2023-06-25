7a3cd2f204330855f49963826e0d70b0590af8d2
akira yamada <akira@arika.org>
2023-06-25 22:40:02 +0900

    Add TEST_QUEUE_SPLIT_GROUPS feature to testunit runner

--- lib/test_queue/runner/testunit.rb
+++ lib/test_queue/runner/testunit.rb
@@ -4,57 +4,65 @@ require_relative '../runner'
 
 gem 'test-unit'
 require 'test/unit'
-require 'test/unit/collector/descendant'
-require 'test/unit/testresult'
-require 'test/unit/testsuite'
+require 'test/unit/collector/load'
 require 'test/unit/ui/console/testrunner'
-
-class Test::Unit::TestSuite
-  attr_accessor :iterator
-
-  def run(result, &progress_block)
-    @start_time = Time.now
-    yield(STARTED, name)
-    yield(STARTED_OBJECT, self)
-    run_startup(result)
-    (@iterator || @tests).each do |test|
-      @n_tests += test.size
-      run_test(test, result, &progress_block)
-      @passed = false unless test.passed?
-    end
-    run_shutdown(result)
-  ensure
-    @elapsed_time = Time.now - @start_time
-    yield(FINISHED, name)
-    yield(FINISHED_OBJECT, self)
-  end
-
-  def failure_count
-    (@iterator || @tests).sum { |t| t.instance_variable_get(:@_result).failure_count }
-  end
-end
 
 module TestQueue
   class Runner
     class TestUnit < Runner
-      def initialize
-        if Test::Unit::Collector::Descendant.new.collect.tests.any?
-          raise 'Do not `require` test files. Pass them via ARGV instead and they will be required as needed.'
+      class TestSuite < ::Test::Unit::TestSuite
+        def initialize(name, iterator)
+          super(name)
+          @tests = IteratorWrapper.new(iterator)
         end
 
+        def run(*)
+          @started = true
+          super
+        end
+
+        def size
+          return 0 unless @started
+
+          super
+        end
+      end
+
+      class IteratorWrapper
+        def initialize(iterator)
+          @generator = Fiber.new do
+            iterator.each do |test|
+              Fiber.yield(test)
+            end
+          end
+        end
+
+        def shift
+          @generator.resume
+        rescue FiberError
+          nil
+        end
+
+        def each
+          while (test = shift)
+            yield(test)
+          end
+        end
+      end
+
+      def initialize
         super(TestFramework::TestUnit.new)
       end
 
       def run_worker(iterator)
-        @suite = Test::Unit::TestSuite.new('specified by test-queue master')
-        @suite.iterator = iterator
+        @suite = TestSuite.new('specified by test-queue master', iterator)
         res = Test::Unit::UI::Console::TestRunner.new(@suite).start
         res.run_count - res.pass_count
       end
 
       def summarize_worker(worker)
         worker.summary = worker.output.split("\n").grep(/^\d+ tests?/).first
-        worker.failure_output = worker.output.scan(/^Failure:\n(.*)\n=======================*/m).join("\n")
+        worker.failure_output = worker.output.scan(/^Failure:[^\n]*\n(.*?)\n=======================*/m).join("\n")
       end
     end
   end
@@ -66,11 +74,39 @@ module TestQueue
       end
 
       def suites_from_file(path)
-        Test::Unit::TestCase::DESCENDANTS.clear
-        require File.absolute_path(path)
-        Test::Unit::Collector::Descendant.new.collect.tests.map { |suite|
-          [suite.name, suite]
-        }
+        test_suite = Test::Unit::Collector::Load.new.collect(path)
+        return [] unless test_suite
+        return test_suite.tests.map { [_1.name, _1] } unless split_groups?
+
+        split_groups(test_suite)
+      end
+
+      def split_groups?
+        return @split_groups if defined?(@split_groups)
+
+        @split_groups = ENV['TEST_QUEUE_SPLIT_GROUPS'] && ENV['TEST_QUEUE_SPLIT_GROUPS'].strip.downcase == 'true'
+      end
+
+      def split_groups(test_suite, groups = [])
+        unless splittable?(test_suite)
+          groups << [test_suite.name, test_suite]
+          return groups
+        end
+
+        test_suite.tests.each do |suite|
+          if suite.is_a?(Test::Unit::TestSuite)
+            split_groups(suite, groups)
+          else
+            groups << [suite.name, suite]
+          end
+        end
+        groups
+      end
+
+      def splittable?(test_suite)
+        test_suite.tests.none? do |test|
+          test.is_a?(Test::Unit::TestCase) && test[:no_split]
+        end
       end
     end
   end
